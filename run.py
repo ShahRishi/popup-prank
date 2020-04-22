5726b456d64ca081b584256ffc4eadd5be128ea9
ShahRishi <shahrishi108@gmail.com>
2020-04-22 15:21:37 -0700

    added main run file

---
+++ run.py
@@ -0,0 +1,31 @@
+from pynput.keyboard import Key, Listener
+from PIL import Image, ImageTk
+import threading
+import tkinter as tk
+
+threads = {}
+
+def Open():
+    window = tk.Tk()
+    window.title("Astley")
+    window.geometry("620x400")
+    window.configure(background='grey')
+    img = ImageTk.PhotoImage(Image.open("astley.jpg"), master=window)
+    panel = tk.Label(window, image=img)
+    panel.pack(side="bottom", fill="both", expand="yes")
+    window.attributes("-topmost", True)
+    window.mainloop()
+
+def on_press(key):
+    try:
+        KEY = key.char.lower()
+        if KEY in ['c', 'e', 'n', 'a']:
+            thread_id = len(threads) + 1
+            threads[thread_id] = threading.Thread(target=Open)
+            threads[thread_id].start()
+    except:
+            pass
+    return True
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
